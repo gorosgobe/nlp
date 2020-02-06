1b18d96e1dc811c496f63be93d77c6f7460d49cb
Pablo Gorostiaga Belio <pg1816@ray17.doc.ic.ac.uk>
2020-02-06 14:21:59 +0000

    hyper search lstm

--- lib/lstm.py
+++ lib/lstm.py
@@ -1,50 +1,56 @@
 import tensorflow.keras
 from tensorflow.keras.models import Sequential, Model
-from tensorflow.keras.layers import Dense, BatchNormalization, Activation, concatenate, Input, LSTM
+from tensorflow.keras.layers import Dense, BatchNormalization, Activation, concatenate, Input, LSTM, Dropout
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 from lib.utils import MODELS_SAVE_PATH, EVALUATION_METRICS, BASE_PADDING
 from lib.data import pad_to_length
 
 import numpy as np
 
-def build_compile_model(english_dimensionality, german_dimensionality):
+def build_compile_model(
+        english_dimensionality, german_dimensionality, learning_rate,
+        layers, dropout, english_lstm_units, german_lstm_units, dropout_lstm
+    ):
     """
     Builds a LSTM model
     """
-
+    #import pdb; pdb.set_trace()
     # define two sets of inputs
     english_input = Input(shape=(None, english_dimensionality), name='english_input')
     german_input = Input(shape=(None, german_dimensionality), name='german_input')
-    
+
     # english branch
-    x = LSTM(100)(english_input)
+    x = LSTM(english_lstm_units)(english_input)
     x = Model(inputs=english_input, outputs=x)
-    
+
     # german branch
-    y = LSTM(100)(german_input)
+    y = LSTM(german_lstm_units)(german_input)
     y = Model(inputs=german_input, outputs=y)
-    
+
     # combine the output of the two branches
     combined = concatenate([x.output, y.output])
-    
+
     # apply a FC layer and then a regression prediction on the
     # combined outputs
-    z = Dense(1000, activation="relu")(combined)
-    z = Dense(300, activation="relu")(z)
-    z = Dense(50, activation="relu")(z)
+    z = combined
+    for units in layers:
+        z = Dense(units, activation="relu")(z)
+        z = Dropout(dropout)(z)
     z = Dense(1, activation="linear", name='output')(z)
-    
+
     # our model will accept the inputs of the two branches and
     # then output a single value
     model = Model(inputs=[x.input, y.input], outputs=z)
     model.compile(
         loss='mean_squared_error',
-        optimizer=tensorflow.keras.optimizers.Adam(learning_rate=0.0001, amsgrad=False),
+        optimizer=tensorflow.keras.optimizers.Adam(learning_rate=learning_rate),
         metrics=EVALUATION_METRICS
     )
     return model
-    
-def fit_model(english_x, german_x, y, batch_size, epochs, english_x_val, german_x_val, y_val, name, seed=2019):
+
+def fit_model(english_x, german_x, y, batch_size, epochs, learning_rate,
+              layers, dropout, english_lstm_units, german_lstm_units,
+              dropout_lstm, english_x_val, german_x_val, y_val, name, seed=2019):
     """
     Builds, compiles and trains model on given dataset
     english_x, german_x: size (7000, max_len_sentence, 100)
@@ -55,9 +61,13 @@ def fit_model(english_x, german_x, y, batch_size, epochs, english_x_val, german_
     tensorflow.compat.v1.set_random_seed(seed)
 
     # TODO: remove 100 hardcoded
-    model = build_compile_model(100, 100)
+    model = build_compile_model(
+        100, 100, learning_rate=learning_rate, layers=layers, dropout=dropout,
+        dropout_lstm=dropout_lstm, english_lstm_units=english_lstm_units,
+        german_lstm_units=german_lstm_units
+    )
     callbacks = [
-        EarlyStopping(monitor='val_loss', patience=40, verbose=1, restore_best_weights=True), 
+        EarlyStopping(monitor='val_loss', patience=40, verbose=1, restore_best_weights=True),
         ModelCheckpoint(f"{MODELS_SAVE_PATH}/{name}.hdf5", monitor='val_loss', verbose=1, save_best_only=True, save_weights_only=True)
     ]
 
@@ -67,10 +77,10 @@ def fit_model(english_x, german_x, y, batch_size, epochs, english_x_val, german_
     if english_x_val is not None and german_x_val is not None and y_val is not None:
         validation_generator = batch_generator(english_x_val, german_x_val, y_val, batch_size)
 
-    model.fit_generator(train_generator, steps_per_epoch=len(english_x)//batch_size, epochs=epochs, 
-                        validation_data=validation_generator, validation_steps=len(english_x_val)//batch_size, 
+    model.fit_generator(train_generator, steps_per_epoch=len(english_x)//batch_size, epochs=epochs,
+                        validation_data=validation_generator, validation_steps=len(english_x_val)//batch_size,
                         verbose=1, callbacks=callbacks)
-    
+
     return model
 
 def eval_model(x_test_english, x_test_german, y_test, model):
@@ -88,8 +98,8 @@ def batch_generator(in1, in2, labels, batch_size=128):
             y_batch = labels[batch_size * i:batch_size * (i + 1)]
 
             # Find max len of sentences in batch and pad vectors
-            in1_longest_sent_len = max(len(x) for x in in1)        
-            in2_longest_sent_len = max(len(x) for x in in2)    
+            in1_longest_sent_len = max(len(x) for x in in1)
+            in2_longest_sent_len = max(len(x) for x in in2)
             pad_to_length(in1_batch_vecs, in1_longest_sent_len, BASE_PADDING)
             pad_to_length(in2_batch_vecs, in2_longest_sent_len, BASE_PADDING)
 
--- train_lstm.py
+++ train_lstm.py
@@ -2,6 +2,8 @@ import lib.data
 import lib.embeddings
 import numpy as np
 import lib.lstm
+import random
+from lib.utils import get_config
 
 if __name__ == "__main__":
 
@@ -27,14 +29,14 @@ if __name__ == "__main__":
     print("Computing training english word embeddings...")
     english_vectors,  _ignored_english_words = lib.embeddings.get_embeddings(
         english_embedding_model,
-        sources_tok, 
+        sources_tok,
         lib.embeddings.EmbeddingType.WORD2VEC
     )
 
     print("Computing training german word embeddings...")
     german_vectors,  _ignored_german_words = lib.embeddings.get_embeddings(
         german_embedding_model,
-        translation_tok, 
+        translation_tok,
         lib.embeddings.EmbeddingType.WORD2VEC
     )
 
@@ -47,28 +49,81 @@ if __name__ == "__main__":
     val_translation_tok, val_translation_vocab = lib.data.tokenize(val_translation)
     val_english_vectors,  ignored_val_english_words = lib.embeddings.get_embeddings(
         english_embedding_model,
-        val_sources_tok, 
+        val_sources_tok,
         lib.embeddings.EmbeddingType.WORD2VEC
     )
 
     print("Computing validation german word embeddings...")
     val_german_vectors,  ignored_val_german_words = lib.embeddings.get_embeddings(
         german_embedding_model,
-        val_translation_tok, 
+        val_translation_tok,
         lib.embeddings.EmbeddingType.WORD2VEC
     )
     print(f"Ignored words, english {len(ignored_val_english_words)}, german {len(ignored_val_english_words)}")
-    
 
-    print("Training model")
-    model = lib.lstm.fit_model(
-        english_x=english_vectors,
-        german_x=german_vectors,
-        y=train_scores, 
-        batch_size=32, 
-        epochs=500, 
-        english_x_val=val_english_vectors, 
-        german_x_val=val_german_vectors,
-        y_val=val_scores, 
-        name='lstm_model_best'
-    )
+    if True:
+
+        print("Hyperparameter search")
+
+        params = {
+            "learning_rate": [0.000001 * x for x in range(1000)],
+            "epochs": [250],
+            "english_lstm": [32, 64, 128, 256, 512],
+            "german_lstm": [32, 64, 128, 256, 512],
+            "batch_size": [32, 64, 128, 256, 512],
+            "dropout": [0.01 * x for x in range(50)],
+            "dropout_lstm": [0.01 * x for x in range(50)],
+            "layers": [random.sample([64, 128, 256, 512, 1024], random.randint(1, 4)) for _ in range(1000)]
+        }
+
+        for _ in range(250):
+            sampled_params = get_config(params)
+            print("Configuration:")
+            print(sampled_params)
+
+            model, history = lib.lstm.fit_model(
+                english_x=english_vectors,
+                german_x=german_vectors,
+                y=train_scores,
+                batch_size=sampled_params['batch_size'],
+                epochs=sampled_params['epochs'],
+                learning_rate=sampled_params['learning_rate'],
+                english_x_val=val_english_vectors,
+                german_x_val=val_german_vectors,
+                y_val=val_scores,
+                name='lstm_model_best',
+                layers=sampled_params["layers"],
+                dropout=sampled_params["dropout"],
+                english_lstm_units=sampled_params["english_lstm"],
+                german_lstm_units=sampled_params["german_lstm"],
+                dropout_lstm=sampled_params["dropout_lstm"]
+            )
+
+            print(history.history["val_mean_squared_error"][-25])
+            val_mean_squared_error = history.history["val_mean_squared_error"][-25]
+            val_pearsonr          =  history.history["val_pearsonr"][-25]
+            val_root_mean_squared_error = history.history["val_root_mean_squared_error"][-25]
+            val_mae = history.history["val_mae"][-25]
+            h = {
+                "val_mean_squared_error": float(val_mean_squared_error),
+                "val_pearsonr": float(val_pearsonr),
+                "val_rmse": float(val_root_mean_squared_error),
+                "val_mae": float(val_mae),
+                **sampled_params
+            }
+            with open("results_lstm.json", "a") as f:
+                f.write(json.dumps(h))
+                f.write("\n")
+    else:
+        print("Training model")
+        model = lib.lstm.fit_model(
+            english_x=english_vectors,
+            german_x=german_vectors,
+            y=train_scores,
+            batch_size=32,
+            epochs=500,
+            english_x_val=val_english_vectors,
+            german_x_val=val_german_vectors,
+            y_val=val_scores,
+            name='lstm_model_best'
+        )
